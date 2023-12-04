ef95f77ffa997bdec43bb920a20aa5843047c4f7
gbadillo <gbadillo@novopayment.com>
2023-12-04 15:37:57 -0500

    Instalacion del websocket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+'use client';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+let socket;
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  useEffect(() => {
+    const socketInit = async () => {
+      await fetch('/api/socket');
+      socket = io(undefined, {
+        path: '/api/my_awesome_socket',
+      });
+
+      socket.on('connect', () => {
+        console.log('Connected', socket.id);
+      });
+
+      socket.on('connect', () => {
+        console.log('connected');
+      });
+    };
+    socketInit();
+  }, []);
+
+  return <h1>Hello, Next.js!</h1>;
 }
