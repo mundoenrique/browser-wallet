9e7aafd4f9fa9809e0a1f689d7b3feaa3c2e8103
elazaro <elazaro@novopayment.com>
2023-12-04 16:44:19 -0500

    change socket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,44 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-let socket;
+
+let socket: any;
 
 export default function Page() {
+  const [apiData, setApiData] = useState<any>(null);
+
   useEffect(() => {
     const socketInit = async () => {
       await fetch('/api/socket');
-      socket = io(undefined, {
-        path: '/api/my_awesome_socket',
+      socket = io('', {
+        path: '/api/my_socket',
       });
 
       socket.on('connect', () => {
         console.log('Connected', socket.id);
       });
 
-      socket.on('connect', () => {
-        console.log('connected');
+      socket.on('movementsChange', (data: any) => {
+        setApiData(data.data);
       });
     };
     socketInit();
   }, []);
 
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <h1>Next.js con Socket.IO y API externa</h1>
+      {apiData && (
+        <div>
+          <h2>Últimos movimientos:</h2>
+          <ul>
+            {apiData.map((movement: any) => (
+              <li key={movement.id}>{movement.description}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,4 +1,8 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import axios from 'axios';
+
+const sockets: Record<string, Socket> = {};
+
 export default function handler(req, res) {
   if (res.socket.server.io) {
     console.log('Server already started!');
@@ -7,12 +11,32 @@ export default function handler(req, res) {
   }
 
   const io = new Server(res.socket.server, {
-    path: '/api/my_awesome_socket',
+    path: '/api/my_socket',
   });
   res.socket.server.io = io;
 
-  const onConnection = (socket) => {
-    console.log('New connection', socket.id);
+  const onConnection = (socket: Socket) => {
+    console.log('Usuario conectado:', socket.id);
+    // Almacenar el socket en un objeto para su referencia posterior
+    sockets[socket.id] = socket;
+
+    // Manejar eventos
+    socket.on('disconnect', () => {
+      console.log('Usuario desconectado:', socket.id);
+      delete sockets[socket.id];
+    });
+
+    // Escuchar la API y emitir cambios a los clientes
+    setInterval(async () => {
+      try {
+        const response = await axios.get('https://api-services-kfmf.onrender.com/cards/4/movements');
+        const data = response.data;
+
+        io.emit('movementsChange', data);
+      } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+      }
+    }, 5000);
   };
 
   io.on('connection', onConnection);
