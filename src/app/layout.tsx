ee2385495c698fad8d2b41ef830d42c1216377e4
hcorredor <hcorredor@novopayment.com>
2023-12-05 16:23:52 -0500

    Agrega metadata en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,30 @@
+import { Metadata } from 'next';
+//Internal app
+import { RootLayout } from '@/interfaces';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: {
+      template: '%s | Indigo',
+      default: 'Indigo',
+    },
+    // manifest: `/api/manifest.webmanifest`,
+    description: 'La billetera digital que te ayudará a pagar, cobrar y ordenar tus finanzas.',
+    icons: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        sizes: '32x32',
+        url: '',
+      },
+    ],
+  };
+}
+
+export default function RootLayout({ children }: RootLayout) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>{children}</body>
     </html>
-  )
+  );
 }
---
+++ src/interfaces/Layout.ts
@@ -0,0 +1,3 @@
+export type RootLayout = {
+  children: React.ReactNode;
+};
