904fca6a316343209a710958b38c4a0055a872a0
hcorredor <hcorredor@novopayment.com>
2023-12-18 15:31:01 -0500

    Ajusta maqueta del dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,5 @@
 'use client';
 
-function ramdomNumber(count: number) {
-  return Math.floor(Math.random() * count);
-}
 export default function Dashboard() {
-  const random = ramdomNumber(2);
-
-  if (random === 1) {
-    throw new Error('Error en componente');
-  }
-
-  return (
-    <>
-      <h1>Dashboard Page</h1>
-      <h2>{random}</h2>
-    </>
-  );
+  return <h1>Dashboard Page</h1>;
 }
