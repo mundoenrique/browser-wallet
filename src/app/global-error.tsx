a801d903f280c03d2ce0900c7dae5b8938669cbb
gerodriguez <gerodriguez@novopayment.com>
2023-12-18 15:35:14 -0400

    Agrega código html en global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -4,11 +4,11 @@ import { ReactNode } from 'react';
 
 export default function GlobalError({ error, reset }: { error: ReactNode; reset: () => void }) {
   return (
-    <>
-      <>
+    <html>
+      <body>
         <Typography variant="h2">{error}</Typography>
         <Button onClick={() => reset()}>Try again</Button>
-      </>
-    </>
+      </body>
+    </html>
   );
 }
