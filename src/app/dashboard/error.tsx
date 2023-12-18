75e3481b9721fd0ce16778c7fa282875e4e395f6
hcorredor <hcorredor@novopayment.com>
2023-12-18 15:00:51 -0500

    Ajustes globales

--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -1,17 +1,11 @@
 'use client';
-import { Button, Typography } from '@mui/material';
-import { useEffect } from 'react';
 
-export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
+import { Typography } from '@mui/material';
 
+export default function ErrorBoundary({ error }: { error: Error }) {
   return (
     <>
-      <Typography variant="h2">Something went wrong!</Typography>
-      <Button onClick={() => reset()}>Try again</Button>
+      <Typography variant="h2">{error.message}</Typography>
     </>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,19 @@
 'use client';
 
+function ramdomNumber(count: number) {
+  return Math.floor(Math.random() * count);
+}
 export default function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  const random = ramdomNumber(2);
+
+  if (random === 1) {
+    throw new Error('Error en componente');
+  }
+
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <h2>{random}</h2>
+    </>
+  );
 }
