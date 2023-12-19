4ab8f046a956175de339efff448149315220c992
gbadillo <gbadillo@novopayment.com>
2023-12-19 10:27:10 -0500

    Agregado tipado para video y canvas

--- src/components/UI/QRCodeReader.tsx
+++ src/components/UI/QRCodeReader.tsx
@@ -1,60 +1,58 @@
 'use client';
-
-import jsQR, { QRCode } from 'jsqr';
 import { useEffect } from 'react';
-
+import jsQR, { QRCode } from 'jsqr';
+//Typado
 import { Point } from 'jsqr/dist/locator';
 
 const decoder = (imageData: ImageData): Promise<QRCode> => {
   return new Promise((resolve) => {
-    const decoded = jsQR(imageData.data, imageData.width, imageData.height);
+    const decoded = jsQR(imageData?.data, imageData?.width, imageData?.height);
     if (decoded) {
       resolve(decoded);
     }
   });
 };
 
-function drawLine(begin: Point, end: Point, color: any, el: CanvasRenderingContext2D) {
-  el.beginPath();
-  el.moveTo(begin.x, begin.y);
-  el.lineTo(end.x, end.y);
-  el.lineWidth = 4;
-  el.strokeStyle = color;
-  el.stroke();
+function drawLine(begin: Point, end: Point, color: string, el: CanvasRenderingContext2D | null) {
+  el?.beginPath();
+  el?.moveTo(begin.x, begin.y);
+  el?.lineTo(end.x, end.y);
+  el ? (el.lineWidth = 4) : null;
+  el ? (el.strokeStyle = color) : null;
+  el?.stroke();
 }
 
 export default function QRCodeReader() {
-  const els: any = {};
-  const setRefFactory = (key: any) => (element: any) => (els[key] = element);
-  let canvasContext: CanvasRenderingContext2D;
+  let videoElement: HTMLVideoElement;
+  let canvasElement: HTMLCanvasElement;
+  let canvasContext: CanvasRenderingContext2D | null;
 
   function runTime() {
-    const { canvas, video } = els;
-    if (video.readyState === video.HAVE_ENOUGH_DATA) {
-      canvas.height = video.videoHeight;
-      canvas.width = video.videoWidth;
-      canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
-      const imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-
-      decoder(imageData).then((code) => {
-        if (code) {
-          drawLine(code.location.topLeftCorner, code.location.topRightCorner, '#FF3B58', canvasContext);
-          drawLine(code.location.topRightCorner, code.location.bottomRightCorner, '#FF3B58', canvasContext);
-          drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, '#FF3B58', canvasContext);
-          drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, '#FF3B58', canvasContext);
-          console.log(code);
-        }
-      });
+    if (videoElement?.readyState === videoElement?.HAVE_ENOUGH_DATA) {
+      canvasElement.height = videoElement?.videoHeight;
+      canvasElement.width = videoElement?.videoWidth;
+      canvasContext?.drawImage(videoElement, 0, 0, canvasElement?.width, canvasElement?.height);
+      const imageData = canvasContext?.getImageData(0, 0, canvasElement?.width, canvasElement?.height);
+      if (imageData) {
+        decoder(imageData).then((code) => {
+          if (code) {
+            drawLine(code.location.topLeftCorner, code.location.topRightCorner, '#FF3B58', canvasContext);
+            drawLine(code.location.topRightCorner, code.location.bottomRightCorner, '#FF3B58', canvasContext);
+            drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, '#FF3B58', canvasContext);
+            drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, '#FF3B58', canvasContext);
+          }
+        });
+      }
     }
     requestAnimationFrame(runTime);
   }
 
   function init() {
     navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }).then((stream) => {
-      const { video } = els;
-      video.srcObject = stream;
-      video.play().then(() => {
-        canvasContext = els.canvas.getContext('2d', { willReadFrequently: true });
+      videoElement.srcObject = stream;
+
+      videoElement.play().then(() => {
+        canvasContext = canvasElement.getContext('2d', { willReadFrequently: true });
         requestAnimationFrame(runTime);
       });
     });
@@ -66,8 +64,20 @@ export default function QRCodeReader() {
 
   return (
     <>
-      <canvas ref={setRefFactory('canvas')} style={{ display: 'block' }}></canvas>
-      <video ref={setRefFactory('video')}></video>
+      <canvas
+        ref={(el: HTMLCanvasElement) => {
+          canvasElement = el;
+        }}
+        style={{ display: 'block' }}
+      ></canvas>
+
+      <video
+        ref={(el: HTMLVideoElement) => {
+          videoElement = el;
+        }}
+        hidden
+        playsInline
+      ></video>
     </>
   );
 }
