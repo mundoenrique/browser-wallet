517a31166a6d5888adef7b249db1fd4b91078b20
jhllerena <jhllerena@novopayment.com>
2024-01-24 11:39:47 -0500

    Crea interfaces

--- src/interfaces/Providers.ts
+++ src/interfaces/Providers.ts
@@ -1,3 +1,13 @@
 export type ProviderProps = {
   children: React.ReactNode;
 };
+
+export type RSAKeysContextType = {
+  publicKey?: string | null;
+  privateKey?: string | null;
+};
+
+export type JWTContextType = {
+  token?: string | null;
+  updateToken?: (newToken: string) => void;
+};
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,6 @@
+export * from './Api';
+export * from './Form';
 export * from './Layout';
 export * from './Providers';
-export * from './Form';
+export * from './User';
 export * from './Validation';
