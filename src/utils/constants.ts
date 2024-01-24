211529b00df53a4a2afc6d5dd9f243e60d6378c9
jhllerena <jhllerena@novopayment.com>
2024-01-24 11:21:55 -0500

    Crea constantes

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const JWT_HEADER = 'x-jwt-auth';
+export const JWS_HEADER = 'x-token';
+export const JWS_ALG = 'RS512';
+export const JWE_ALG = 'RSA-OAEP-256';
+export const ISSUER = 'Emisor';
+export const AUDIENCE = 'Audiencia';
