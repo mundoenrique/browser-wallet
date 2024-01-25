0e3dc965550e2d119a5b29f880931a64f49cd1ef
hcorredor <hcorredor@novopayment.com>
2024-01-25 15:36:27 -0500

    Ajusta maqueta del Stepper

--- src/app/(Onboarding)/signup/components/SignupStepper.tsx
+++ src/app/(Onboarding)/signup/components/SignupStepper.tsx
@@ -1,17 +1,21 @@
 'use client';
 
 //Internal app
-import { GradientContainer, NavExternal } from '@/components';
+import { NavExternal } from '@/components';
+import { Box } from '@mui/material';
 
 export default function SignupStepper(props: { step: number; children: JSX.Element[] }) {
   const { step, children } = props;
 
   return (
-    <>
-      <GradientContainer disable={step > 0}>
-        <NavExternal image relative />
-        {children[step]}
-      </GradientContainer>
-    </>
+    <Box
+      sx={{
+        background: step > 0 ? 'none' : 'linear-gradient(35deg, rgba(146,218,142,0) 20%, rgba(172,255,167,0.6) 100%)',
+        minHeight: '100vh',
+      }}
+    >
+      <NavExternal image relative />
+      {children[step]}
+    </Box>
   );
 }
