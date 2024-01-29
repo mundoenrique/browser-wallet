12dcd99873489a4869da3340b7508310a4b89283
hcorredor <hcorredor@novopayment.com>
2024-01-29 15:44:51 -0500

    Comenta codigo sin uso

--- src/app/api/logger/route.ts
+++ src/app/api/logger/route.ts
@@ -1,21 +1,21 @@
-import { getServerSession } from 'next-auth/next';
+// import { getServerSession } from 'next-auth/next';
 import { NextRequest, NextResponse } from 'next/server';
 //Internal app
-const logger = require('@/utils/logger');
-import { options } from '@/utils/nextAuth';
+// const logger = require('@/utils/logger');
+// import { options } from '@/utils/nextAuth';
 import { decrypt, encryptToView } from '@/utils/encrypt_decrypt';
 
 export async function POST(req: NextRequest) {
   const { payload } = await req.json();
-  const reqData = JSON.parse(decrypt({ data:payload }));
-  const session = await getServerSession(options);
+  const reqData = JSON.parse(decrypt({ data: payload }));
+  // const session = await getServerSession(options);
   const ip = req.headers.get('x-forwarded-for');
 
-  if (session) {
-    logger[reqData.type]({ message: reqData.msg, user: session.user?.name, ip });
-  } else {
-    logger[reqData.type]({ message: reqData.msg });
-  }
+  // if (session) {
+  //   logger[reqData.type]({ message: reqData.msg, user: session.user?.name, ip });
+  // } else {
+  //   logger[reqData.type]({ message: reqData.msg });
+  // }
 
   const encryption = encryptToView({ code: 0, msg: 'ok' });
 
