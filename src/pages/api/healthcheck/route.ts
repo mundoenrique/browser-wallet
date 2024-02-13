0c3cad1c00e5a65b6c6bfacf4ba06315e1105444
Edinson Cabello <ecabello@novopayment.com>
2024-02-13 13:54:03 -0400

    Actualiza ubicacion api logger

---
+++ src/pages/api/healthcheck/route.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type ResponseData = {
+  message: string
+}
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  res.status(200).json({ message: 'PRUEBA DE HEALTHCHECK OK' });
+}
