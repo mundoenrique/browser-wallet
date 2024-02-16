85826b3de56e04fde1e59bbcc1e1bf17bc80c4ba
jhllerena <jhllerena@novopayment.com>
2024-02-16 15:36:16 -0500

    Agrega interface api

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,6 +2,4 @@ export * from './api';
 export * from './components';
 export * from './constant';
 export * from './form';
-export * from './providers';
-export * from './user';
 export * from './validation';
