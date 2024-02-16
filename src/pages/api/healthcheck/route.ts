45761c166d93788a74612500d74e7a8bcec2531c
Edinson Cabello <ecabello@novopayment.com>
2024-02-16 09:36:39 -0400

    Actualiza mensaje

--- src/pages/api/healthcheck/route.ts
+++ src/pages/api/healthcheck/route.ts
@@ -5,5 +5,5 @@ type ResponseData = {
 }
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-  res.status(200).json({ message: 'PRUEBA DE HEALTHCHECK OK' });
+  res.status(200).json({ message: 'Health check ok test' });
 }
