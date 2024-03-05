5e4f58cc2e074b28adee7da3e23d163f00977e8e
hcorredor <hcorredor@novopayment.com>
2024-03-05 13:41:54 -0500

    Incorpora maqueta para identificacion del consultor

--- src/app/(Pages)/identify/[user]/page.tsx
+++ src/app/(Pages)/identify/[user]/page.tsx
@@ -1,7 +1,10 @@
 import LogoGreen from '%/images/LogoGreen';
 import { PurpleLayout } from '@/components';
 
-export default function page() {
+export default function page({ params }: any) {
+  const { user } = params;
+  console.log('🚀 ~ page ~ user:', user);
+
   return (
     <PurpleLayout>
       <LogoGreen />
