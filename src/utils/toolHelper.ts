8ad5c5a623a52b85cf7ac6f1824940a0ca7dec71
jcadiz <jcadiz@novopayment.com>
2024-03-18 19:42:11 -0400

    Añade función copyToClipboard en toolhelper

--- src/utils/toolHelper.ts
+++ src/utils/toolHelper.ts
@@ -8,3 +8,16 @@ export function stringAvatar(name: string) {
     children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
   };
 }
+
+/**
+ * Copies the specified text to the clipboard using the `navigator.clipboard.writeText` method.
+ *
+ * @param text - The text to be copied to the clipboard.
+ */
+export const copyToClipboard = async (text: any) => {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (error) {
+    console.error('Error copying text:', error);
+  }
+};
