f722f5839da5e517e8ac3141069b076c9888489b
jcadiz <jcadiz@novopayment.com>
2024-03-19 14:18:08 -0400

    Añade función handleDownload en toolhelper

--- src/utils/toolHelper.ts
+++ src/utils/toolHelper.ts
@@ -1,3 +1,5 @@
+import html2canvas from 'html2canvas';
+
 /**
  * Handle text initials on the avatar
  *
@@ -20,4 +22,24 @@ export const copyToClipboard = async (text: any) => {
   } catch (error) {
     console.error('Error copying text:', error);
   }
+};
+
+/**
+ * Handles the download of an image based on the specified parameters.
+ *
+ * @param element - The HTML element to be captured as an image.
+ * @param downloadName - The name of the downloaded file.
+ * @param backgroundColor - The background color for the captured image.
+ */
+export const handleDownload = async (element: HTMLElement, fileName: string, backgroundColor: string) => {
+  const canvas = await html2canvas(element, {
+    removeContainer: false,
+    allowTaint: true,
+    backgroundColor: backgroundColor,
+  });
+  const image = canvas.toDataURL('image/png');
+  const link = document.createElement('a');
+  link.href = image;
+  link.download = fileName;
+  link.click();
 };
