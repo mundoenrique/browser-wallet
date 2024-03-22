eb0f6e2dd4499d71b9ed98d77466ad6b948ae748
elazaro <elazaro@novopayment.com>
2024-03-22 12:40:19 -0500

    comments

--- src/app/api/apiGee/connect/route.ts
+++ src/app/api/apiGee/connect/route.ts
@@ -1,13 +1,4 @@
-import {
-  JWS_HEADER,
-  decryptJWE,
-  getEnvVariable,
-  handleApiGeeRequest,
-  handleApiGeeResponse,
-  handleApiRequest,
-  handleApiResponse,
-  handleJWT,
-} from '@/utils';
+import { JWS_HEADER, decryptJWE, getEnvVariable, handleApiGeeRequest, handleApiGeeResponse, handleJWT } from '@/utils';
 import { connect } from '@/utils/apiGeeServer';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -22,7 +13,6 @@ export async function POST(request: NextRequest) {
   const jwtPayload = await handleJWT(request, jweApiPublicKey);
   const jweAppPublicKey = jwtPayload.jwePublicKey;
   const jwsAppPublicKey = jwtPayload.jwsPublicKey;
-  // const { jweAppPublicKey, jwsAppPublicKey } = await handleJWT(request, jweApiPublicKey);
   const data = await decryptJWE(payload.data, jweApiPrivateKey);
 
   const { jwe, jws } = await handleApiGeeRequest(data);
@@ -34,12 +24,9 @@ export async function POST(request: NextRequest) {
       const response = await connect('post', url, request.headers, { data: jwe });
       const encryptedResponse = await handleApiGeeResponse(response.data, jweAppPublicKey, jwsAppPublicKey);
       console.log('encryptedResponse: ', encryptedResponse);
-
-      // return NextResponse.json(encryptedResponse, { status: response.status });
       return encryptedResponse;
-      return NextResponse.json({ message: 'Ha ocurrido un error' }, { status: 200 });
+      // return NextResponse.json({ message: 'Ha ocurrido un error' }, { status: 200 });
     } catch (error) {
-      // console.log('Ha ocurrido un error: ', error);
       return NextResponse.json({ message: 'Ha ocurrido un error' }, { status: 500 });
     }
   }
@@ -47,16 +34,23 @@ export async function POST(request: NextRequest) {
 }
 
 export async function GET(request: NextRequest) {
-  console.log('GET request: ');
-
+  console.log('GET');
   const url = request.headers.get('x-url');
   request.headers.delete('x-url');
+
+  const jweApiPublicKey = getEnvVariable('JWE_PUBLIC_KEY');
+  const jweApiPrivateKey = getEnvVariable('JWE_PRIVATE_KEY');
+
+  const jwtPayload = await handleJWT(request, jweApiPublicKey);
+  const jweAppPublicKey = jwtPayload.jwePublicKey;
+  const jwsAppPublicKey = jwtPayload.jwsPublicKey;
 
   if (url) {
     try {
       const response = await connect('get', url, request.headers);
-      return NextResponse.json(response.data, { status: response.status });
-      // Falta verificar si la data viene cifrada, descifrarla
+
+      const encryptedResponse = await handleApiGeeResponse(response.data, jweAppPublicKey, jwsAppPublicKey);
+      return encryptedResponse;
     } catch (error) {
       console.log('Ha ocurrido un error: ', error);
       return NextResponse.json({ message: 'Ha ocurrido un error' }, { status: 500 });
--- src/app/api/apiGee/gettoken/route.ts
+++ src/app/api/apiGee/gettoken/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from '@/utils/apiGeeServer';
 
+/** GET /api/gettoken
+* @param {NextRequest} request
+* @returns {Promise<NextResponse>}
+* @description Get token from Apigee
+
+**/
 export async function POST(request: NextRequest) {
   const tokenResponse = await getToken();
   const { access_token } = tokenResponse;
