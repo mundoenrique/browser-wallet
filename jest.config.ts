af14b4eab2cfbd865a59e6ee8a0f5f65a408b7df
Edinson Cabello <ecabello@novopayment.com>
2024-04-04 15:51:34 -0400

    Actualiza cobertura de pruebas

--- jest.config.ts
+++ jest.config.ts
@@ -16,17 +16,11 @@ const config: Config = {
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/app/**/signin/*page.tsx',
-    'src/app/**/password-recover/*page.tsx',
-    'src/app/**/signup/*page.tsx',
     'src/app/**/dashboard/*page.tsx',
-    'src/app/**/dashboard/legal/*page.tsx',
-    'src/app/**/dashboard/change-password/*page.tsx',
     'src/app/**/dashboard/**/frequent-questions/*page.tsx',
-    'src/app/**/dashboard/recharge/*page.tsx',
-    'src/app/**/dashboard/debt/*page.tsx',
     'src/app/**/dashboard/card-configuration/*page.tsx',
-    'src/app/**/dashboard/transfer/*page.tsx',
+    'src/app/**/dashboard/recharge/*page.tsx',
+    'src/app/**/signup/*page.tsx',
   ],
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
   coverageProvider: 'v8',
@@ -34,10 +28,15 @@ const config: Config = {
   reporters: ['default', ['jest-junit', { outputDirectory: 'coverage/junit' }]],
   testPathIgnorePatterns: [
     '<rootDir>/__tests__/tools/',
+    '<rootDir>/__tests__/unit/Dashboard/change-password',
+    '<rootDir>/__tests__/unit/Dashboard/clients',
+    '<rootDir>/__tests__/unit/Dashboard/collect',
+    '<rootDir>/__tests__/unit/Dashboard/debt',
+    '<rootDir>/__tests__/unit/Dashboard/legal',
     '<rootDir>/__tests__/unit/Dashboard/movements',
-    '<rootDir>/__tests__/unit/Dashboard/help/Help.test.tsx',
-     '<rootDir>/__tests__/unit/Dashboard/clients',
-    '<rootDir>/__tests__/unit/Dashboard/collect'
+    '<rootDir>/__tests__/unit/Dashboard/transfer',
+    '<rootDir>/__tests__/unit/PasswordRecover',
+    '<rootDir>/__tests__/unit/Signin/Recover',
   ],
 };
 
