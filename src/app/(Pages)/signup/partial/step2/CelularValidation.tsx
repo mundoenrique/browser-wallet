f766d6c157e112e19dbba9753d74d336a7ad3ad1
gbadillo <gbadillo@novopayment.com>
2024-04-08 08:05:33 -0500

    AJuste Modale Errror

--- src/app/(Pages)/signup/partial/step2/CelularValidation.tsx
+++ src/app/(Pages)/signup/partial/step2/CelularValidation.tsx
@@ -3,18 +3,20 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Box, Button } from '@mui/material';
 //internal app
 import { getSchema } from '@/config';
 import { useRegisterStore, useUiStore } from '@/store';
-import { Box, Button } from '@mui/material';
 import InputOTP from '@/components/form/InputOTP';
 import { CardStep } from '..';
+import { ModalError } from '@/components';
 
 export default function CelularValidation() {
   const schema = getSchema(['otp']);
   const [optUuid, setOtpUuid] = useState<string>('');
   const { inc, dec, onboardingUuid } = useRegisterStore();
   const { setLoadingScreen } = useUiStore();
+  const { requestError, setRequestError } = useState<boolean>(false);
 
   const { handleSubmit, control } = useForm({
     defaultValues: { otp: '' },
@@ -27,6 +29,7 @@ export default function CelularValidation() {
     await new Promise((resolve) => {
       setTimeout(resolve, 1000);
     });
+    setRequestError(true);
     //TODO: Validar documentacion
     //https://sandbox-api.novopayment.com/api/v0/onboarding/{onboardingId}/tfa
     await fetch(`/api/v1/onboarding/tfa/${onboardingUuid}`, {
@@ -71,36 +74,39 @@ export default function CelularValidation() {
   }, [requestTFACode]);
 
   return (
-    <CardStep stepNumber="2">
-      <Box
-        component="form"
-        onSubmit={handleSubmit(onSubmit)}
-        sx={{ display: 'flex', flexDirection: 'column', flex: 1, justifyContent: 'space-between' }}
-      >
-        <Box sx={{ textAlign: 'center', mb: 5 }}>
-          <InputOTP
-            name="otp"
-            title="Por tu seguridad validaremos tu celular"
-            text="Ingresa el código de 4 dígitos que te enviamos por SMS al *** *** 1214"
-            length={4}
-            control={control}
-          />
-        </Box>
+    <>
+      <CardStep stepNumber="2">
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ display: 'flex', flexDirection: 'column', flex: 1, justifyContent: 'space-between' }}
+        >
+          <Box sx={{ textAlign: 'center', mb: 5 }}>
+            <InputOTP
+              name="otp"
+              title="Por tu seguridad validaremos tu celular"
+              text="Ingresa el código de 4 dígitos que te enviamos por SMS al *** *** 1214"
+              length={4}
+              control={control}
+            />
+          </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 3 / 2, mb: { xs: 3, sm: 0 } }}>
-          <Button
-            variant="outlined"
-            onClick={() => {
-              dec();
-            }}
-          >
-            Anterior
-          </Button>
-          <Button variant="primary" type="submit">
-            Siguiente
-          </Button>
+          <Box sx={{ display: 'flex', justifyContent: 'center', gap: 3 / 2, mb: { xs: 3, sm: 0 } }}>
+            <Button
+              variant="outlined"
+              onClick={() => {
+                dec();
+              }}
+            >
+              Anterior
+            </Button>
+            <Button variant="primary" type="submit">
+              Siguiente
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </CardStep>
+      </CardStep>
+      {requestError && <ModalError title="otp" description="error" />}
+    </>
   );
 }
