ab2b485b0209e5dd8247cffa3705c27236a6aadc
Enrique Peñaloza <epenaloza@novopayment.com>
2024-04-09 06:41:25 -0400

    Prueba redis

--- src/app/api/redis/route.ts
+++ src/app/api/redis/route.ts
@@ -4,14 +4,19 @@ import { createRedisInstance } from '@/utils/redis';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-
   const redis = createRedisInstance();
 
   try {
     await redis.set(`session:ConectOk`, JSON.stringify({ accessToken: 'token' }));
-    return new NextResponse(JSON.stringify({ redis: 'ok'}), {
-      status: 200,
-    });
+    return new NextResponse(
+      JSON.stringify({
+        host: `REDIS_HOST ${process.env.REDIS_HOST}`,
+        web: `WEB_ENV ${process.env.WEB_ENV}`,
+      }),
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.error('Error setting Redis session:', error);
     return new NextResponse(JSON.stringify({ error: 'Failed to set session' }), {
