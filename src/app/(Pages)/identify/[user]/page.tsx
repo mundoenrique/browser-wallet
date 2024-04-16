eb5ddadaaed6d0c843eec4798a872f3df17ed98f
hcorredor <hcorredor@novopayment.com>
2024-04-16 08:51:00 -0500

    Ajusta componente identify

--- src/app/(Pages)/identify/[user]/page.tsx
+++ src/app/(Pages)/identify/[user]/page.tsx
@@ -3,28 +3,18 @@ import DataUser from './partial/DataUser';
 import { NotFoundError } from '@/components';
 import { createRedisInstance } from '@/utils/redis';
 
-export default function UserPage(params: any) {
-  console.log('PARAMS: ', params)
+export default async function UserPage(params: any) {
   const redis = createRedisInstance();
-  const user = params.user;
-  console.log('USER: ', user)
+  const user = params.params.user;
 
   // Add null checks for params and user
-  if (!params || !params.user) return <NotFoundError code={404} />;
+  if (!params.params || !params.params.user) return <NotFoundError code={404} />;
 
-  try {
-    const userData = redis.get(`${user}`);
-    const data = JSON.stringify(userData)
-    // Check if userData exists before deleting and quitting
-    if (data) {
-      redis.del(`${data}`);
-      redis.quit();
-    }
+  const userData = await redis.get(`${user}`);
+  redis.del(`${user}`);
+  redis.quit();
 
-    if (!data) return <NotFoundError code={404} />;
+  if (!userData) return <NotFoundError code={404} />;
 
-    return <DataUser user={data} />;
-  } catch (error) {
-    console.log(error);
-  }
+  return <DataUser user={userData} />;
 }
