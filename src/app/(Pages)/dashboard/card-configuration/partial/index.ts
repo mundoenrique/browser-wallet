3afb9922a467378cd7114369c0017e2ec4a74f88
jcadiz <jcadiz@novopayment.com>
2024-05-29 13:20:35 -0400

    Actualización de rutas de archivos

--- src/app/(Pages)/dashboard/card-configuration/partial/index.ts
+++ src/app/(Pages)/dashboard/card-configuration/partial/index.ts
@@ -1,7 +1,7 @@
 export { default as Main } from './Main';
 export { default as BlockCard } from './BlockCard';
 export { default as ChangePin } from './ChangePin';
-export { default as Survey } from './deleteAccount/Survey';
+export { default as Survey } from './DeleteAccount/Survey';
 export { default as RequestPhysicalCard } from './RequestPhysicalCard';
-export { default as DeleteAccount } from './deleteAccount/DeleteAccount';
+export { default as DeleteAccount } from './DeleteAccount/DeleteAccount';
 export { default as ActivatePhysicalCard } from './ActivatePhysicalCard';
