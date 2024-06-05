430c1695af610ade6e94e01edd45223860ccf3ef
hcorredor <hcorredor@novopayment.com>
2024-06-05 10:47:24 -0500

    Ajusta estado

--- src/app/(Pages)/dashboard/debt/partial/Errors.tsx
+++ src/app/(Pages)/dashboard/debt/partial/Errors.tsx
@@ -2,14 +2,14 @@
 
 import Image from 'next/image';
 import { useEffect } from 'react';
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Box, Button, ClickAwayListener, Stack, Typography } from '@mui/material';
 //Internal app
 import { useDebStore } from '@/store';
 import { PurpleLayout } from '@/components';
 import { fuchsiaBlue } from '@/theme/theme-default';
 
 export default function Error() {
-  const { setView } = useDebStore();
+  const setView = useDebStore((state) => state.setView);
 
   const error = useDebStore((state) => state.error);
 
@@ -39,30 +39,38 @@ export default function Error() {
   const description = setErrorsParams().description;
   const iconName = setErrorsParams().iconName || 'sorry';
 
+  const handleClickAway = () => {
+    setView('DEBT');
+  };
+
   return (
-    <PurpleLayout hidePelca>
-      <Box sx={{ width: { xs: 'auto', sm: 542 }, display: 'grid', justifyItems: 'center', textAlign: 'center', mx: 3 }}>
-        <Box mb={11 / 2}>
-          <Image src={`/images/arts/pet-${iconName}.png`} height={179} width={172} alt="Error" priority />
-        </Box>
-        <Stack spacing={3 / 2} mb={4} textAlign="center">
-          <Typography variant="subtitle1" color={fuchsiaBlue[50]}>
-            {title}
-          </Typography>
-
-          <Typography variant="body2" color={fuchsiaBlue[50]}>
-            {description}
-          </Typography>
-        </Stack>
-
-        <Button
-          variant="text"
-          onClick={() => setView('DEBT')}
-          sx={{ textDecoration: 'underline', fontWeight: '700', color: 'white' }}
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <PurpleLayout hidePelca>
+        <Box
+          sx={{ width: { xs: 'auto', sm: 542 }, display: 'grid', justifyItems: 'center', textAlign: 'center', mx: 3 }}
         >
-          Volver a intentar
-        </Button>
-      </Box>
-    </PurpleLayout>
+          <Box mb={11 / 2}>
+            <Image src={`/images/arts/pet-${iconName}.png`} height={179} width={172} alt="Error" priority />
+          </Box>
+          <Stack spacing={3 / 2} mb={4} textAlign="center">
+            <Typography variant="subtitle1" color={fuchsiaBlue[50]}>
+              {title}
+            </Typography>
+
+            <Typography variant="body2" color={fuchsiaBlue[50]}>
+              {description}
+            </Typography>
+          </Stack>
+
+          <Button
+            variant="text"
+            onClick={() => setView('DEBT')}
+            sx={{ textDecoration: 'underline', fontWeight: '700', color: 'white' }}
+          >
+            Volver a intentar
+          </Button>
+        </Box>
+      </PurpleLayout>
+    </ClickAwayListener>
   );
 }
--- src/app/(Pages)/dashboard/debt/partial/Success.tsx
+++ src/app/(Pages)/dashboard/debt/partial/Success.tsx
@@ -2,7 +2,7 @@
 
 import Dollar from '@mui/icons-material/AttachMoney';
 import Clock from '@mui/icons-material/QueryBuilder';
-import { Avatar, Box, Card, Divider, Stack, Typography } from '@mui/material';
+import { Avatar, Box, Card, ClickAwayListener, Divider, Stack, Typography } from '@mui/material';
 //Internal app
 import { useDebStore } from '@/store';
 import { EsikaIsotipo } from '%/Icons';
@@ -11,6 +11,7 @@ import { fuchsiaBlue } from '@/theme/theme-default';
 import { CardTicket, ContainerLayout, Linking, PurpleLayout } from '@/components';
 
 export default function Success() {
+  const setView = useDebStore((state) => state.setView);
   const payOffDebt = useDebStore((state) => state.payOffDebt);
 
   const description = [
@@ -31,52 +32,58 @@ export default function Success() {
     },
   ];
 
+  const handleClickAway = () => {
+    setView('DEBT');
+  };
+
   return (
-    <PurpleLayout hidePelca bigModal left confetti width="calc(100% - 315px)">
-      <ContainerLayout>
-        <Typography
-          variant="h6"
-          sx={{ display: 'block', color: 'white', mb: 4, mt: { xs: 4, md: 0 }, textAlign: 'center' }}
-        >
-          Comprobante
-        </Typography>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <PurpleLayout hidePelca bigModal left confetti width="calc(100% - 315px)">
+        <ContainerLayout>
+          <Typography
+            variant="h6"
+            sx={{ display: 'block', color: 'white', mb: 4, mt: { xs: 4, md: 0 }, textAlign: 'center' }}
+          >
+            Comprobante
+          </Typography>
 
-        <CardTicket textBotton="Guardar" download>
-          <Box sx={{ display: 'grid', width: '100%' }}>
-            <Typography variant="subtitle1" textAlign="center">
-              Has pagado
-            </Typography>
-            <Typography variant="h4" color="primary" textAlign="center" fontWeight={700}>
-              ¡Felicidades!
-            </Typography>
-            <Typography variant="caption" textAlign="center">
-              Los datos de la transacción son:
-            </Typography>
-            <Typography variant="body1" color="primary" textAlign="center" mb={3} fontWeight={700}>
-              {payOffDebt?.transactionIdentifier}
-            </Typography>
-            <Card sx={{ boxShadow: 'none', p: 1 }}>
-              <Stack direction="column" divider={<Divider orientation="horizontal" />} spacing={1}>
-                {description.map((item, i) => (
-                  <Box key={i} sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Avatar sx={{ bgcolor: fuchsiaBlue[200], width: 26, height: 26, mr: 1 }}>{item.icon}</Avatar>
-                    <Box sx={{ display: 'grid', alignItems: 'center' }}>
-                      <Typography fontSize={10}>{item.label}</Typography>
-                      <Typography variant="caption" fontWeight={700}>
-                        {item.description}
-                      </Typography>
+          <CardTicket textBotton="Guardar" download>
+            <Box sx={{ display: 'grid', width: '100%' }}>
+              <Typography variant="subtitle1" textAlign="center">
+                Has pagado
+              </Typography>
+              <Typography variant="h4" color="primary" textAlign="center" fontWeight={700}>
+                ¡Felicidades!
+              </Typography>
+              <Typography variant="caption" textAlign="center">
+                Los datos de la transacción son:
+              </Typography>
+              <Typography variant="body1" color="primary" textAlign="center" mb={3} fontWeight={700}>
+                {payOffDebt?.transactionIdentifier}
+              </Typography>
+              <Card sx={{ boxShadow: 'none', p: 1 }}>
+                <Stack direction="column" divider={<Divider orientation="horizontal" />} spacing={1}>
+                  {description.map((item, i) => (
+                    <Box key={i} sx={{ display: 'flex', alignItems: 'center' }}>
+                      <Avatar sx={{ bgcolor: fuchsiaBlue[200], width: 26, height: 26, mr: 1 }}>{item.icon}</Avatar>
+                      <Box sx={{ display: 'grid', alignItems: 'center' }}>
+                        <Typography fontSize={10}>{item.label}</Typography>
+                        <Typography variant="caption" fontWeight={700}>
+                          {item.description}
+                        </Typography>
+                      </Box>
                     </Box>
-                  </Box>
-                ))}
-              </Stack>
-            </Card>
-          </Box>
-        </CardTicket>
+                  ))}
+                </Stack>
+              </Card>
+            </Box>
+          </CardTicket>
 
-        <Box sx={{ width: '100%', display: { xs: 'flex', md: 'none' }, justifyContent: 'center' }}>
-          <Linking href="/dashboard" label="Volver al inicio" hidenArrow color="white" underline fontSize={16} />
-        </Box>
-      </ContainerLayout>
-    </PurpleLayout>
+          <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }} onClick={() => setView('DEBT')}>
+            <Linking href="/dashboard" label="Volver al inicio" hidenArrow color="white" underline fontSize={16} />
+          </Box>
+        </ContainerLayout>
+      </PurpleLayout>
+    </ClickAwayListener>
   );
 }
