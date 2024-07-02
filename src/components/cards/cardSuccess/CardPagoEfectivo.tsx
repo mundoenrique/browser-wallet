45c10d8d8cc9aa1bea4d89dd7d7608b0bf72133b
hcorredor <hcorredor@novopayment.com>
2024-07-02 15:02:22 -0500

    Ajusta maqueta

--- src/components/cards/cardSuccess/CardPagoEfectivo.tsx
+++ src/components/cards/cardSuccess/CardPagoEfectivo.tsx
@@ -108,17 +108,17 @@ export default function CardPagoEfectivo({
             <Typography fontWeight={700} mb={3}>
               Yape, Plin u otras billeteras:
             </Typography>
-            <picture>
+            <Box>
               {codeQr ? (
-                <>
+                <picture>
                   <img src={codeQr} alt="Qr Code" width={106} height={106} />
-                </>
+                </picture>
               ) : (
-                <>
+                <picture>
                   <img src={Qr} alt="Qr Code" width={106} height={106} />
-                </>
+                </picture>
               )}
-            </picture>
+            </Box>
           </Box>
         </CardReport>
       </Box>
