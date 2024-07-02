f7cff031436352d03ddaa4215631a9528114e8d6
hcorredor <hcorredor@novopayment.com>
2024-07-02 11:35:41 -0500

    Ajusta imagen codigo qr

--- src/components/cards/cardSuccess/CardPagoEfectivo.tsx
+++ src/components/cards/cardSuccess/CardPagoEfectivo.tsx
@@ -96,7 +96,11 @@ export default function CardPagoEfectivo({ cip, children, label, download, share
               Yape, Plin u otras billeteras:
             </Typography>
             <Box>
-              <Image src={codeQr ? codeQr : Qr} alt="Qr Code" width={106} height={106} />
+              {codeQr ? (
+                <Image src={codeQr} alt="Qr Code" width={106} height={106} priority />
+              ) : (
+                <Image src={Qr} alt="Qr Code" width={106} height={106} priority />
+              )}
             </Box>
           </Box>
         </CardReport>
