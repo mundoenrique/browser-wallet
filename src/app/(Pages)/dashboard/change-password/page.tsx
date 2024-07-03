d96a7156e2429d9dd5892ac84a385fadcc215fc7
hcorredor <hcorredor@novopayment.com>
2024-07-03 11:56:41 -0500

    Agrega tageo

--- src/app/(Pages)/dashboard/change-password/page.tsx
+++ src/app/(Pages)/dashboard/change-password/page.tsx
@@ -2,28 +2,31 @@
 
 import { useForm } from 'react-hook-form';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Box, Button, Typography } from '@mui/material';
+import { sendGTMEvent } from '@next/third-parties/google';
 //Internal app
+import { api } from '@/utils/api';
 import { getSchema } from '@/config';
 import ModalOtp from '@/components/modal/ModalOtp';
-import { useNavTitleStore, useMenuStore, useUserStore, useUiStore } from '@/store';
 import { ContainerLayout, InputPass, ModalResponsive } from '@/components';
-import { api } from '@/utils/api';
-import { useRouter } from 'next/navigation';
+import { useNavTitleStore, useMenuStore, useUserStore, useUiStore, useHeadersStore } from '@/store';
 
 export default function ChangePassword() {
   const router = useRouter();
+
+  const schemaFormPassword = getSchema(['newPassword', 'newPasswordConfirmation', 'currentPassword']);
 
   const { setCurrentItem } = useMenuStore();
 
   const { updateTitle } = useNavTitleStore();
 
+  const host = useHeadersStore((state) => state.host);
+
   const [openRc, setOpenRc] = useState<boolean>(false);
 
   const [openOtp, setOpenOtp] = useState<boolean>(false);
-
-  const schemaFormPassword = getSchema(['newPassword', 'newPasswordConfirmation', 'currentPassword']);
 
   const userId = useUserStore((state) => state.user.userId);
 
@@ -33,6 +36,20 @@ export default function ChangePassword() {
     defaultValues: { newPassword: '', newPasswordConfirmation: '', currentPassword: '' },
     resolver: yupResolver(schemaFormPassword),
   });
+
+  useEffect(() => {
+    sendGTMEvent({
+      event: 'ga4.trackEvent',
+      eventName: 'page_view_ga4',
+      eventParams: {
+        page_location: `${host}/dashboard/change-password`,
+        page_title: 'Yiro :: cambiarContraseña',
+        page_referrer: `${host}/dashboard`,
+        section: 'Yiro :: cambiarContraseña',
+        previous_section: 'dashboard',
+      },
+    });
+  }, [host]);
 
   const onSubmit = async () => {
     setOpenOtp(true);
@@ -57,6 +74,20 @@ export default function ChangePassword() {
         setLoadingScreen(false);
         reset();
       });
+
+    sendGTMEvent({
+      event: 'ga4.trackEvent',
+      eventName: 'select_content',
+      eventParams: {
+        content_type: 'boton_modal',
+        section: 'Yiro :: cambiarContraseña',
+        previous_section: 'dashboard',
+        selected_content: 'Verificar',
+        destination_page: `${host}/dashboard/change-password`,
+        pop_up_type: 'Cambiar contraseña',
+        pop_up_title: 'Verificación en dos pasos',
+      },
+    });
   };
 
   const redirect = () => {
@@ -92,7 +123,24 @@ export default function ChangePassword() {
           <InputPass name="currentPassword" control={control} label="Ingresar tu contraseña actual" />
           <InputPass name="newPassword" control={control} label="Ingresa una nueva contraseña" />
           <InputPass name="newPasswordConfirmation" control={control} label="Confirma tu nueva contraseña" />
-          <Button variant="contained" type="submit" fullWidth>
+          <Button
+            variant="contained"
+            type="submit"
+            fullWidth
+            onClick={() => {
+              sendGTMEvent({
+                event: 'ga4.trackEvent',
+                eventName: 'select_content',
+                eventParams: {
+                  content_type: 'boton',
+                  section: 'Yiro :: cambiarContraseña',
+                  previous_section: 'dashboard',
+                  selected_content: 'Guardar',
+                  destination_page: `${host}/dashboard/change-password`,
+                },
+              });
+            }}
+          >
             Guardar
           </Button>
         </Box>
@@ -100,7 +148,22 @@ export default function ChangePassword() {
 
       <ModalOtp
         open={openOtp}
-        handleClose={() => setOpenOtp(false)}
+        handleClose={() => {
+          setOpenOtp(false);
+          sendGTMEvent({
+            event: 'ga4.trackEvent',
+            eventName: 'select_content',
+            eventParams: {
+              content_type: 'boton_modal',
+              section: 'Yiro :: cambiarContraseña',
+              previous_section: 'dashboard',
+              selected_content: 'Cerrar',
+              destination_page: `${host}/dashboard/change-password`,
+              pop_up_type: 'Cambiar contraseña',
+              pop_up_title: 'Verificación en dos pasos',
+            },
+          });
+        }}
         onSubmit={onSubmitOtp}
         processCode="CHANGE_PASSWORD_OTP"
       />
