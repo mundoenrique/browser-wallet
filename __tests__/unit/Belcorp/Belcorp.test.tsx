b0a7e6b820b3f695af3e2de84e4465abe109537f
Edinson Cabello <ecabello@novopayment.com>
2024-07-03 13:39:55 -0400

    Elimina codigo innecesario

--- __tests__/unit/Belcorp/Belcorp.test.tsx
+++ __tests__/unit/Belcorp/Belcorp.test.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { render, act, screen, fireEvent, waitFor } from '@testing-library/react';
 import Belcorp from '@/app/(Pages)/belcorp/page';
 import { renderInput } from '../../tools/unitTestHelper.test';
-import { api } from '@/utils/api';
-
-jest.mock('@/utils/api');
-const mockApi = api as jest.Mocked<typeof api>;
 
 jest.mock('next/navigation', () => ({
   useRouter: () => ({
@@ -17,16 +13,10 @@ describe('Belcorp component', () => {
   let codeConsultant: HTMLInputElement;
   let codeCountry: HTMLButtonElement;
   let submitButton: HTMLElement;
-  const userData = {
-    consultant: '12345678',
-    country: 'PE',
-  };
-  mockApi.get.mockResolvedValue({ status: 200, data: { data: userData } });
-  const onSubmitMock = jest.fn();
 
   beforeEach(async () => {
     await act(async () => {
-      render(<Belcorp onSubmit={onSubmitMock}/>);
+      render(<Belcorp />);
     });
     codeConsultant = screen.getByLabelText('Código de la consultora');
     codeCountry = screen.getByLabelText('País');
@@ -40,7 +30,6 @@ describe('Belcorp component', () => {
   });
 
   it('calls onSubmit function when form is submitted', async () => {
-    const { consultant, country } = userData;
     const fetchMock = jest.fn().mockResolvedValue({
       json: () => Promise.resolve({ data: 'https://example.com' }),
     });
