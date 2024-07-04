ef89a4b3160f3e30ba113d0b0b6cdb6efe34134c
Edinson Cabello <ecabello@novopayment.com>
2024-07-04 18:41:51 -0400

    Ajusta pruebas unitarias dashboard

--- __tests__/unit/Dashboard/Dashboard.test.tsx
+++ __tests__/unit/Dashboard/Dashboard.test.tsx
@@ -1,27 +1,19 @@
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
 // Internal app
 import Dashboard from '@/app/(Pages)/dashboard/page';
 import { mockRouterPush } from '../../tools/unitTestHelper.test';
+import { api } from '@/utils/api';
 
 jest.mock('@/store', () => ({
   ...jest.requireActual('@/store'),
-  useUiStore: jest.fn(() => ({
-    setModalError: jest.fn(),
-    setLoadingScreen: jest.fn(),
-    setErrorModal: jest.fn(),
-    setReloadFunction: jest.fn(),
-  })),
   useUserStore: jest.fn(() => ({
     getUserCardId: jest.fn().mockReturnValue('mockedCardId'),
     user: { userId: 'mockedUserId', firstName: 'John' },
   })),
 }));
 
-jest.mock('@/utils/api', () => ({
-  api: {
-    get: jest.fn().mockResolvedValue({ data: { data: [] } }),
-  },
-}));
+jest.mock('@/utils/api');
+const mockApi = api as jest.Mocked<typeof api>;
 
 //** Define the mock component within jest.mock call
 jest.mock('@/components/cards/cardInformation/CardInformation', () => {
@@ -31,7 +23,9 @@ jest.mock('@/components/cards/cardInformation/CardInformation', () => {
 });
 
 describe('Dashboard', () => {
+  const movementsData = { id: 1, amount: 10 };
   const routerPushMock = jest.fn();
+  mockApi.get.mockResolvedValue({ status: 200, data: { data: movementsData } });
 
   beforeEach(async () => {
     mockRouterPush(routerPushMock);
@@ -49,17 +43,34 @@ describe('Dashboard', () => {
     expect(screen.getByText('Últimos movimientos')).toBeInTheDocument();
   });
 
-  //** Navigates to /dashboard/debt on CardDebt click
-  it('should navigate to /dashboard/debt on CardDebt click', () => {
-    const cardDebt = screen.getByText('Pagar');
-    fireEvent.click(cardDebt);
-    expect(routerPushMock).toHaveBeenCalledWith('/dashboard/debt');
+  it('call API getMovements', async () => {
+    await mockApi.get(`/cards/123456/transactions`, { params: { days: 90, limit: 5 } })
+    await waitFor(() => {
+      expect(mockApi.get).toHaveBeenCalled();
+      expect(mockApi.get).toHaveBeenCalledWith(`/cards/123456/transactions`, { params: { days: 90, limit: 5 } });
+    });
   });
 
-  //** Navigates to /dashboard/clients on OweMe CardDebt click
-  it('should navigate to /dashboard/clients on OweMe CardDebt click', () => {
-    const oweMeCardDebt = screen.getByText('Gestionar');
-    fireEvent.click(oweMeCardDebt);
-    expect(routerPushMock).toHaveBeenCalledWith('/dashboard/clients');
+  it('call API getMovements error', async () => {
+      mockApi.get.mockImplementation(() => Promise.reject(new Error('API error')));
+      await waitFor(() => {
+        expect(mockApi.get).toHaveBeenCalled();
+      });
+    });
+
+  describe('Navigations', () => {
+    //** Navigates to /dashboard/debt on CardDebt click
+    it('should navigate to /dashboard/debt on CardDebt click', () => {
+      const cardDebt = screen.getByText('Pagar');
+      fireEvent.click(cardDebt);
+      expect(routerPushMock).toHaveBeenCalledWith('/dashboard/debt');
+    });
+
+    //** Navigates to /dashboard/clients on OweMe CardDebt click
+    it('should navigate to /dashboard/clients on OweMe CardDebt click', () => {
+      const oweMeCardDebt = screen.getByText('Gestionar');
+      fireEvent.click(oweMeCardDebt);
+      expect(routerPushMock).toHaveBeenCalledWith('/dashboard/clients');
+    });
   });
 });
