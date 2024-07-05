606e651349ec42f538e0d5dc140a8eb32f01d9a5
hcorredor <hcorredor@novopayment.com>
2024-07-05 10:08:54 -0500

    Ajusta cors

--- next.config.js
+++ next.config.js
@@ -18,8 +18,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
