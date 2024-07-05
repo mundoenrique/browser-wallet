48e4a7ae8bf0fc276b1f35cfc13734e821b54da9
hcorredor <hcorredor@novopayment.com>
2024-07-05 09:57:49 -0500

    Ajusta cors

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+  crossOrigin: 'anonymous',
+  poweredByHeader: false,
+  output: 'standalone',
+  images: {
+    domains: ['pre1a.niubizqr.pagoefectivo.pe'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pre1a.niubizqr.pagoefectivo.pe',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
