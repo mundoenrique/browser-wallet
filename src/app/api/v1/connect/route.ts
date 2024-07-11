0702270a2eb188d8123b9654780ed0966e6e6226
ylopez <ylopez@novopayment.com>
2024-07-11 10:00:36 -0500

    Elimina refreshTime

--- src/app/api/v1/connect/route.ts
+++ src/app/api/v1/connect/route.ts
@@ -1,34 +1,19 @@
 import { NextRequest } from 'next/server';
-import { cookies } from 'next/headers'
 //Internal app
 import { HandleCustomerRequest } from '@/utils/apiGeeServer';
-import { createRedisInstance } from '@/utils';
-import { SESSION_ID } from '@/utils/constants';
 
 export async function GET(request: NextRequest) {
-  refreshTime();
   return HandleCustomerRequest(request);
 }
 
 export async function POST(request: NextRequest) {
-  refreshTime();
   return HandleCustomerRequest(request);
 }
 
 export async function PUT(request: NextRequest) {
-  refreshTime();
   return HandleCustomerRequest(request);
 }
 
 export async function PATCH(request: NextRequest) {
-  refreshTime();
   return HandleCustomerRequest(request);
-}
-
-function refreshTime() {
-  const uuidCookie = cookies().get(SESSION_ID)?.value || null
-  if (uuidCookie) {
-    const redis = createRedisInstance();
-    redis.expire(`session:${uuidCookie}`, 300);
-  }
 }
