0f82ada6b11e1d8fb0d032c129ef20a42121ad68
Edinson Cabello <ecabello@novopayment.com>
2024-07-12 16:37:34 -0400

    Actualiza pruebas unitarias de recarga

--- __tests__/unit/Dashboard/recharge/Recharge.test.tsx
+++ __tests__/unit/Dashboard/recharge/Recharge.test.tsx
@@ -7,7 +7,7 @@ import { api } from '@/utils/api';
 jest.mock('@/store', () => ({
   ...jest.requireActual('@/store'),
   useUserStore: jest.fn(() => ({
-    getUserPhone: jest.fn(() => jest.fn()),
+    getUserPhone: jest.fn(() => '123456789'),
     user: { userId: 'mockedUserId', firstName: 'John' },
   })),
   useCollectStore: jest.fn(() => ({ setLinkData: jest.fn() })),
@@ -51,41 +51,6 @@ describe('Recharge', () => {
     //** Displays an error message when the user submits the form with an empty password field.
     it('should display an error message for empty recharge field', async () => {
       emptyField(submitButton, 'Campo obligatorio');
-    });
-  });
-
-  describe('onSubmit function and call apis', () => {
-    it('submit form and call api post', async () => {
-      fireEvent.change(amountInput, { target: { value: '100.00' } });
-      fireEvent.click(submitButton);
-
-      // const payload = {
-      //   fullName: 'Jhon Doe',
-      //   phoneNumber: jest.fn(() => { return { plaintext: '123456789' }}),
-      //   operationCode: 'DESTINATION_CHARGE',
-      //   providerCode: 'PAGO_EFECTIVO',
-      //   currencyCode: 'PEN',
-      //   amount: amountInput.value,
-      // };
-
-      const payload = jest.fn (() => {
-        return {
-          fullName: 'Jhon Doe',
-          phoneNumber: jest.fn(() => { return '123456789' }),
-          operationCode: 'DESTINATION_CHARGE',
-          providerCode: 'PAGO_EFECTIVO',
-          currencyCode: 'PEN',
-          amount: amountInput.value,
-        }
-      })
-
-      await mockApi.post(`/payments/051999541/charge`, payload);
-
-      await waitFor(() => {
-        expect(mockApi.post).toHaveBeenCalledTimes(1);
-        expect(mockApi.post).toHaveBeenCalledWith('/payments/051999541/charge', payload);
-      });
-
     });
   });
 
