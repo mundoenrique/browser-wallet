47cac4e41d602cd4fb20652d2b58265949acf2b6
ylopez <ylopez@novopayment.com>
2024-07-17 13:26:19 -0500

    Elimina archivos.

--- src/store/wedding/jwt.slice.ts
+++
@@ -1,9 +0,0 @@
-import { StateCreator } from 'zustand';
-//Internal app
-import { JwtStoreProps } from '@/interfaces/store';
-
-export const createJwtSlice: StateCreator<JwtStoreProps> = (set) => ({
-  token: null,
-
-  setToken: (token: string | null) => set({ token }),
-});
