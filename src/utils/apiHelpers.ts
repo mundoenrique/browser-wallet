cd83915dc385d28da53d1248a630b50851880430
ylopez <ylopez@novopayment.com>
2024-08-05 11:21:39 -0500

    Ajusta cifrado cookie.

--- src/utils/apiHelpers.ts
+++ src/utils/apiHelpers.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import logger from './logger';
 import { handleApiResponse } from '.';
 import { encryptForge } from './toolHelper';
-import { JWT_HEADER, JWS_HEADER, SESSION_ID } from './constants';
+import { JWT_HEADER, JWS_HEADER, SESSION_ID, REDIS_CIPHER } from './constants';
 import { IApiGeeResponse, IEncryptedBody, IJWTPayload } from '@/interfaces';
 import { verifyJWT, encryptJWE, decryptJWE, signJWE, verifyDetachedJWS } from './jwt';
 
@@ -140,9 +140,7 @@ export async function handleResponse(
 
     if (isBrowser) {
       var expires = (new Date(Date.now()+ 86400*1000)).toUTCString();
-      cookieSet = `${SESSION_ID}=${encryptForge(
-        responseObj.data.sessionId
-      )}; HttpOnly=true; Path=/; Secure=true; SameSite=true; Expires=${expires};`;
+      cookieSet = `${SESSION_ID}=${encryptForge(responseObj.data.sessionId, REDIS_CIPHER)}; HttpOnly=true; Path=/; Secure=true; SameSite=true; Expires=${expires};`;
     }
 
     const response = NextResponse.json(encryptedResponse, {
