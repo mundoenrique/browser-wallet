d1afe93194c41cafec0e1d131b067765596b64f1
hcorredor <hcorredor@novopayment.com>
2024-09-23 09:54:14 -0500

    Ajusta logica

--- src/app/(Pages)/dashboard/debt/page.tsx
+++ src/app/(Pages)/dashboard/debt/page.tsx
@@ -1,18 +1,10 @@
 'use client';
 
-import { useEffect } from 'react';
-//Internal app
 import { useDebStore } from '@/store';
 import { Success, Error, Debt, Congratulations } from './partial';
 
 export default function MyDebt() {
-  const { debt, view, setView } = useDebStore();
-
-  useEffect(() => {
-    if (debt.amount <= 0) {
-      setView('CONGRATULATIONS');
-    }
-  }, [debt]); //eslint-disable-line
+  const { view } = useDebStore();
 
   const configDebtRoutes = (page: any) => {
     const routes: { [key: string]: any } = {
