6477b3d8616e0ffb698b7b32ac7a2c2d27b346a4
ylopez <ylopez@novopayment.com>
2024-10-21 09:48:03 -0500

    Incorpora Api para renovar sessión

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(request: NextRequest) {
   const { url, nextUrl } = request;
   const pathname = nextUrl.pathname;
   const partsUrl = pathname.split('/');
-  const protectedApiV1 = ['/api/v1/connect', '/api/v1/gettoken', '/api/v1/setcode', '/api/v1/redis'];
+  const protectedApiV1 = ['/api/v1/connect', '/api/v1/gettoken', '/api/v1/setcode', '/api/v1/redis', '/api/v1/activeSession'];
   const protectedApiV0 = ['onboarding', 'catalogs', 'payments', 'users', 'cards'];
   const protectedRoutes = ['signin', 'signup', 'password-recover', 'dashboard'];
 
