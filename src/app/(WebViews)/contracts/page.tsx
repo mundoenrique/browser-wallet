41c579eb2ae37c2c37e1077d7261aaf7f34c9093
elazaro <elazaro@novopayment.com>
2024-12-12 13:24:33 -0500

    shared

--- src/app/(WebViews)/contracts/page.tsx
+++ src/app/(WebViews)/contracts/page.tsx
@@ -2,16 +2,14 @@
 import { Box } from '@mui/material';
 
 //Internal app
-import { Conditions, Terms, Policies, ContainerLayout } from '@/components';
+import { Conditions, Terms, Policies } from '@/components';
 
 export default function page() {
   return (
-    <ContainerLayout fullWidth>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-        <Terms />
-        <Conditions />
-        <Policies />
-      </Box>
-    </ContainerLayout>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+      <Terms />
+      <Conditions />
+      <Policies />
+    </Box>
   );
 }
