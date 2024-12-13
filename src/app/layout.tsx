337b008e8b6a93e5f9a3716ef88249d138780ab5
hcorredor <hcorredor@novopayment.com>
2024-12-13 17:18:41 -0500

    Ajusta layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,28 @@
+import { Metadata } from 'next';
 import { GoogleTagManager } from '@next/third-parties/google';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 //Internal app
 import { ChildrenProps } from '@/interfaces';
 import GlobalErrorMessage from '@/components/layout/GlobalErrorMessage';
 import { HydrationProvider, KeyProvider, LoadingScreen, MuiProvider } from '@/components';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: {
+      template: '%s | Yiro',
+      default: 'Yiro',
+    },
+    description: 'La billetera digital que te ayudará a pagar, cobrar y ordenar tus finanzas.',
+    icons: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        sizes: '32x32',
+        url: '/images/favicon.ico',
+      },
+    ],
+  };
+}
 
 export default function RootLayout({ children }: Readonly<ChildrenProps>) {
   return (
